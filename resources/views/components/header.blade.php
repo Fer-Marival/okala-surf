acb68285d781ba3cecd87f7cfdf429c223c9c611
Fernando Uribe <furibe@marivalaccess.com>
2019-11-12 06:36:56 -0600

    favicon

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -20,3 +20,19 @@
 		</div>
 	</div>
 </div>
+<div id="tab-bar">
+	<div class="row no-gutters align-items-center">
+		<div class="col">
+			<a href="tel:+3222899180"><i class="fas fa-phone-volume"></i></a>
+		</div>
+		<div class="col">
+			<a href="mailto:okalasurfschool@gmail.com?subject=Contacto de sitio web mobile"><i class="fas fa-envelope-open-text"></i></a>
+		</div>
+		<div class="col">
+			<a href="https://wa.me/3222899180?text=I'm%20interested%20in%20your%20car%20for%20sale"><i class="fab fa-whatsapp"></i></a>
+		</div>
+		<div class="col">
+			<a href="/booking" class="btn-hover color-9">@lang('global.book')</a>
+		</div>
+	</div>
+</div>
---
+++ resources/views/components/seo.blade.php
@@ -0,0 +1,85 @@
+        <meta name="description" content="renta de cámaras, paseos a la playa colomitos, las  animas,  cerro del mono, renta de equipo de snorkel y kayaks renta de bicicletas, host para tus vacaciones, traslados al aeropuerto o al hotel en puerto vallarta y riviera nayarit.">
+        <link rel="dns-prefetch" href="https://www.gstatic.com" />
+        <link rel="dns-prefetch" href="https://www.facebook.com" />
+        <link rel="dns-prefetch" href="https://www.gtrk.s3.amazonaws.com" />
+        <meta name="dc.subject" content="Vdtrips" />
+        <meta name="dc.title" content="Renta de cámaras en Puerto Vallarta y Rieviera nayarit, Mexico" />
+        <meta name="dc.description" content="Renta de cámaras, paseos a la playa colomitos, las  animas,  cerro del mono, renta de equipo de snorkel y kayaks renta de bicicletas, host para tus vacaciones, traslados al aeropuerto o al hotel en puerto vallarta y riviera nayarit." />
+        <meta name="dc.source" content="http://www.vdtrips.com/" />
+
+        <meta property="og:locale" content="es"/>
+        <meta property="og:locale:alternate" content="en" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Vdtrips Vallarta Daniel's Trips" />
+        <meta property="og:description" content="Renta de cámaras, paseos a la playa colomitos, las  animas,  cerro del mono, renta de equipo de snorkel y kayaks renta de bicicletas, host para tus vacaciones, traslados al aeropuerto o al hotel en puerto vallarta y riviera nayarit." />
+        <meta property="og:url" content="https://www.vdtrips.com/" />
+        <meta property="og:site_name" content="vdtrips" />
+        <meta property="og:image" content="https://www.vdtrips.com/img/logo.svg" />
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136318483-1"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-136318483-1');
+        </script>
+        <script type="application/ld+json">
+        {
+          "@context": "http://schema.org",
+          "@type": "TouristTrip",
+          "name": "Vdtrips Vallarta Daniel's trips",
+          "description": "Renta de cámaras, paseos a la playa colomitos, las  animas,  cerro del mono, renta de equipo de snorkel y kayaks renta de bicicletas, host para tus vacaciones, traslados al aeropuerto o al hotel en puerto vallarta y riviera nayarit.",
+          "openingHours": "Mo-Sun 07:00-23:00",
+          "telephone": "+52013221684772",
+          "address":
+          {
+          "@type": "PostalAddress",
+          "streetAddress": "Rio Rhin 128",
+          "addressLocality": "Puerto Vallarta",
+          "addressRegion": "JAL",
+          "addressCountry": "MX"
+          },
+        }
+        {
+          "@context": "http://schema.org",
+          "@type": "TouristTrip",
+          "name": "Caminata a la playa",
+          "description": "Disfruta de las  mejores playas escondidas en puerto vallarta,  nosotros te llevamos y te hacemos disfrutar al máximo, renta de cámaras, paseos a la playa colomitos,  las  animas,  cerro del mono, renta de equipo de snorkel y kayaks renta de bicicletas, host para tus vacaciones, traslados al aeropuerto o al hotel  riviera nayarit  ",
+          "touristType": [
+            "Urban tourism",
+            "Cultural tourism"
+          ],
+          "subjectOf": {
+            "@type": "TouristTrip",
+            "name": "Paseo a colomitos",
+            "url": ""
+          },
+          "itinerary": {
+            "@type": "ItemList",
+            "numberOfItems": 2,
+            "itemListElement": [
+              {
+                "@type": "ListItem",
+                "position": 1,
+                "item":
+                {
+                  "@type": "TouristAttraction",
+                  "name": "Paseo a PLaya colomitos",
+                  "description": "Conoce una de las playas escondidas mas hermosas de puerto vallarta y cabo corrientes, playa colomitos ubicado a 10 min caminando de la playa de boca de tomatlán, disfruta con nuestra caminata, hermosa experiencia con el mar y la naturaleza en la playa colomitos."
+                }
+              },
+              {
+                "@type": "ListItem",
+                "position": 2,
+                "item":
+                {
+                  "@type": "TouristAttraction",
+                  "name": "Caminata a la playa las ánimas",
+                  "description": "Conoce unas de las playas escondidas más famosas de puerto vallarta y cabo corrientes, la  playa las  animas, disfruta de  la comida a  la orilla del mar y conoce en el camino playas vírgenes,  conectate con la  naturaleza y el mar y disfruta de esta hermosa  caminata."
+                }
+              }
+            ]
+          }
+        }
+    </script>
